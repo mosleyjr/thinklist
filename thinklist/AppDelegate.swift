8c049576fa658964a899d3badcced36ee2397368
mosleyjr <mosleyjr.services@gmail.com>
2015-06-10 09:57:46 -0600

    Initial Commit

---
+++ thinklist/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  thinklist
+//
+//  Created by Jason Mosley on 6/10/15.
+//  Copyright (c) 2015 Jason Mosley. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ thinklistTests/thinklistTests.swift
@@ -0,0 +1,36 @@
+//
+//  thinklistTests.swift
+//  thinklistTests
+//
+//  Created by Jason Mosley on 6/10/15.
+//  Copyright (c) 2015 Jason Mosley. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class thinklistTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
